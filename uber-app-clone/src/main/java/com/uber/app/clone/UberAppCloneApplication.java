51547e3e44400c014943203e033437f02d060052
mrFarooque <farooqueansari85@gmail.com>
2024-08-10 11:33:38 +0530

    project create

---
+++ uber-app-clone/src/main/java/com/uber/app/clone/UberAppCloneApplication.java
@@ -0,0 +1,13 @@
+package com.uber.app.clone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UberAppCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UberAppCloneApplication.class, args);
+	}
+
+}
---
+++ uber-app-clone/src/test/java/com/uber/app/clone/UberAppCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uber.app.clone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UberAppCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
